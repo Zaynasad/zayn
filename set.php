32b44f86a2b0e9f2e46513c25251cbe0e3dba323
Asadzayn <112749109+Zaynasad@users.noreply.github.com>
2022-09-28 11:30:07 +0530

    Add files via upload

---
+++ set.php
@@ -0,0 +1,20 @@
+<?php
+///////////////////////////////////////////////////////
+///////// Mohammad asad khan /////////////////
+///////////////////////////////////////////////////////
+
+# Creator: ZAYNKHAN (ASAD)
+# Telegram: zaynkhan91
+
+
+# username  to get followers 
+# Enter the original account username
+$youser="__mohammadasad";
+
+# Username fake account 
+# Enter the username of the account that was sacrificed
+$t_user="gajauzbzbs";
+
+# Passwoard/ fake account 
+# enter the password of the account that was sacrificed
+$t_pass="Asad123@";
